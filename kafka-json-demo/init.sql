9d28b9665c0e81f7b458e75418506fd3efed2f01
earthchen <youwei.chen@duobei.com>
2021-05-27 18:18:45 +0800

    feat(kafka-json-sql): 读取 kafka json 数据写入 mysql

--- kafka-json-demo/init.sql
+++ kafka-json-demo/init.sql
@@ -1,13 +1,37 @@
-CREATE TABLE KafkaTable (
-                            `user_id` BIGINT,
-                            `item_id` BIGINT,
-                            `behavior` STRING,
-                            `ts` TIMESTAMP(3) METADATA FROM 'timestamp'
-) WITH (
+CREATE TABLE KafkaTable
+(
+    `user_id` BIGINT,
+    `item_id` BIGINT,
+    `behavior` STRING
+)
+WITH (
     'connector' = 'kafka',
     'topic' = 'user_behavior',
-    'properties.bootstrap.servers' = '123.57.82.220:31090,123.57.82.220:31091,123.57.82.220:31092',
+    'properties.bootstrap.servers' = '127.0.0.1:9000',
     'properties.group.id' = 'testGroup',
     'scan.startup.mode' = 'latest-offset',
     'format' = 'json'
 )
+
+
+CREATE TABLE kafka_sink_table
+(
+    `user_id` BIGINT,
+    `item_id` BIGINT,
+    `behavior` STRING
+)
+WITH (
+    'connector' = 'jdbc',
+    'url' = 'jdbc:mysql://127.0.0.1:3306/test',
+    'username'= 'root'
+    'password'= '123456'
+    'table-name' = 'kafka_sink_table'
+);
+
+create table kafka_sink_table
+(
+    `id`       bigint primary key auto_increment,
+    `user_id`  bigint not null,
+    `item_id`  bigint not null,
+    `behavior` varchar(256)
+);
--- kafka-json-demo/src/main/java/io/github/earthchen/json/KafkaJsonMain.java
+++ kafka-json-demo/src/main/java/io/github/earthchen/json/KafkaJsonMain.java
@@ -4,6 +4,8 @@ import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.TableEnvironment;
 
+import static org.apache.flink.table.api.Expressions.$;
+
 
 /**
  * @author earthchen
@@ -23,10 +25,10 @@ public class KafkaJsonMain {
                 ") WITH (\n" +
                 "    'connector' = 'kafka',\n" +
                 "    'topic' = 'user_behavior1',\n" +
-                "    'properties.bootstrap.servers' = '123.57.82.220:31090,123.57.82.220:31091,123.57.82.220:31092'," +
+                "    'properties.bootstrap.servers' = '127.0.0.1'," +
                 "\n" +
                 "    'properties.group.id' = 'testGroup',\n" +
-                "    'scan.startup.mode' = 'latest-offset',\n" +
+                "    'scan.startup.mode' = 'group-offsets',\n" +
                 "    'json.fail-on-missing-field' = 'false'," +
                 "    'json.ignore-parse-errors' = 'true'," +
                 "    'format' = 'json'\n" +
@@ -34,47 +36,25 @@ public class KafkaJsonMain {
         Table kafkaJsonSource = tEnv.from("kafka_source");
         tEnv.executeSql("CREATE TABLE print_table WITH ('connector' = 'print')\n" +
                 "LIKE kafka_source (EXCLUDING ALL)");
-        tEnv.executeSql("select user_id as UserId from kafka_source").print();
-        // kafkaJsonSource.select($("user_id").as("userId"),
-        //         $("item_id"),
-        //         $("behavior")).executeInsert("print_table");
 
+        tEnv.executeSql("CREATE TABLE kafka_sink_table\n" +
+                "(\n" +
+                "    `user_id` BIGINT,\n" +
+                "    `item_id` BIGINT,\n" +
+                "    `behavior` STRING\n" +
+                ")\n" +
+                "WITH (\n" +
+                "    'connector' = 'jdbc',\n" +
+                "    'url' = 'jdbc:mysql://127.0.0.1:3306/test',\n" +
+                "    'username'= 'root',\n" +
+                "    'password'= '123456',\n" +
+                "    'table-name' = 'kafka_sink_table'\n" +
+                ")");
         // tEnv.executeSql("select * from kafka_source").print();
-        //
-        // tEnv.executeSql("CREATE TABLE kafka_source (" +
-        //         "    funcName STRING," +
-        //         "    data ROW<snapshots ARRAY<ROW<content_type STRING,url STRING>>,audio ARRAY<ROW<content_type " +
-        //         "STRING,url STRING>>>," +
-        //         "    resultMap ROW<`result` MAP<STRING,STRING>,isSuccess BOOLEAN>," +
-        //         "    meta  MAP<STRING,STRING>," +
-        //         "    `type` INT," +
-        //         "    `timestamp` BIGINT," +
-        //         "    arr ARRAY<ROW<address STRING,city STRING>>," +
-        //         "    map MAP<STRING,INT>," +
-        //         "    doublemap MAP<STRING,MAP<STRING,INT>>," +
-        //         "    proctime as PROCTIME()" +
-        //         ") WITH (" +
-        //         "    'connector' = 'kafka'," +
-        //         "    'topic' = 'test_kafka_json'," +
-        //         "    'properties.bootstrap.servers' = '123.57.82.220:31090,123.57.82.220:31091,123.57.82
-        //         .220:31092'," +
-        //         "    'properties.group.id' = 'test_kafka_json'," +
-        //         "    'scan.startup.mode' = 'latest-offset'," +
-        //         "    'format' = 'json'," +
-        //         "    'json.fail-on-missing-field' = 'true'," +
-        //         "    'json.ignore-parse-errors' = 'false'" +
-        //         ")");
-        //
-        // Table kafkaJsonSource = tEnv.from("kafka_source");
-        // tEnv.executeSql("select * from kafka_source").print();
-        // tEnv.executeSql("select" +
-        //         "funcName," +
-        //         "doublemap['inner_map']['key']," +
-        //         // "count(data.snapshots[1].url)," +
-        //         "`type`," +
-        //         "TUMBLE_START(proctime, INTERVAL '30' second) as t_start" +
-        //         "from kafka_source" +
-        //         "group by TUMBLE(proctime, INTERVAL '30' second),funcName,`type`,doublemap['inner_map']['key']")
-        //         .print();
+
+        kafkaJsonSource.select($("user_id"),
+                $("item_id"),
+                $("behavior")).executeInsert("kafka_sink_table");
+
     }
 }
